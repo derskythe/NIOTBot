bf0d65dfcbb2553cd0eb362f433541365090c940
DerSkythe <skif@skif.ws>
2023-04-27 19:05:29 +0400

    master → master (2e530ab) ∆ M:1 A:1 @ rb00-itd012 M: NiotTelegramBot/NiotTelegramBot.csproj | 82 +++++++++++++++++----------------- A: NiotTelegramBot/fdfd.cs |

---
+++ NiotTelegramBot/fdfd.cs
@@ -0,0 +1,26 @@
+﻿public class Pipe : MonoBehaviour
+{
+    private Character character;
+
+// Это нужно для инициализации    
+    void Start()
+    {
+        character = FindObjectOfType();
+    }
+
+// Update вызывается один раз на кадр    
+    void Update()
+    {
+        if (character.transform.position.x - transform.position.x > 30)
+        {
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            character.Death();
+        }
+    }
+}
